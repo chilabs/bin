f84e17434ef2a5fc188a78405da041a6cd8b828a
x4xie <x4xie@engmail.uwaterloo.ca>
2011-07-07 02:39:36 -0400

    Reorganized scripts based on OS and also added mysql uninstall script for osx.

---
+++ osx/uninstall_mysql.sh
@@ -0,0 +1,7 @@
+sudo rm /usr/local/mysql
+sudo rm -rf /usr/local/mysql*
+sudo rm -rf /Library/StartupItems/MySQLCOM
+sudo rm -rf /Library/PreferencePanes/My*
+sudo rm -rf /Library/Receipts/mysql*
+sudo rm -rf /Library/Receipts/MySQL*
+sudo rm /etc/my.cnf
